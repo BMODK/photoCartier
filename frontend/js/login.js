ae4db0f42e813055c3d1da4ba8aa0fde3532fa50
Carla Souza <carlavanessasp275@gmail.com>
2026-05-23 19:58:33 -0300

    criação do formulario de cadastro

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -3,6 +3,10 @@ const botaoLogin = document.querySelector('#botaologin');
 const inputNome = document.querySelector('input[type="text"]');
 const botaoSign = document.querySelector('#botaosign');
 const inputSenha = document.querySelector('input[type="password"]');
+const containerLogin = document.querySelector('#container-login');
+const containerSign = document.querySelector('#container-cadastro');
+const botaoCadastrar = document.querySelector('#botao-cadastro');
+
 
 botaoLogin.addEventListener('click', function(){
     if(inputNome.value === "Teste" && inputSenha.value === "123"){
@@ -10,4 +14,14 @@ botaoLogin.addEventListener('click', function(){
     }else{
         alert("Erro: Usuário ou Senha invalidos");
     }
+});
+botaoSign.addEventListener('click', function(){
+    //Esconde o formulario de login
+    containerLogin.classList.add('escondido');
+    //Mostra o formulario de cadastro
+    containerSign.classList.remove('escondido');
+});
+botaoCadastrar.addEventListener('click', function(){
+    containerLogin.classList.remove('escondido');
+    containerSign.classList.add('escondido');
 });
