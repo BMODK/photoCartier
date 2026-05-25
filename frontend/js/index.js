e4aedfee73ea8a64f262379ab10c12449cd170d9
vanessa-py <carlavanessasp275@gmail.com>
2026-05-25 12:43:54 -0300

    adicionando efeito a fonte da página index

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -19,3 +19,46 @@ botaoTema.addEventListener('click', function(){
         document.querySelector('#botao-tema i').classList.add('fa-sun');
     }
 });
+
+//logica para o texto do hero
+
+
+const elementoTexto = document.getElementById('texto-maquina');
+
+
+const textoCompleto = "PHOTOCARTIER";
+let indiceAtual = 0;
+let estaApagando = false;
+
+function motorMaquinaEscrever() {
+    // Pega o pedaço do texto baseado no índice atual
+    if (!estaApagando) {
+        // Se está digitando, pega uma letra a mais
+        elementoTexto.textContent = textoCompleto.substring(0, indiceAtual + 1);
+        indiceAtual++;
+    } else {
+        // Se está apagando, remove uma letra
+        elementoTexto.textContent = textoCompleto.substring(0, indiceAtual - 1);
+        indiceAtual--;
+    }
+
+    // Define a velocidade padrão da digitação (em milissegundos)
+    let velocidade = estaApagando ? 50 : 150; // Apaga mais rápido do que digita
+
+    // LOGICA DOS ESTADOS (Terminou de digitar? Terminou de apagar?)
+    if (!estaApagando && indiceAtual === textoCompleto.length) {
+        // Terminou a frase! Dá uma pausa de 2 segundos antes de começar a apagar
+        velocidade = 2000; 
+        estaApagando = true;
+    } else if (estaApagando && indiceAtual === 0) {
+        // Apagou tudo! Espera meio segundo e volta a digitar
+        velocidade = 500;
+        estaApagando = false;
+    }
+
+    // Executa a própria função novamente após o tempo determinado (o loop)
+    setTimeout(motorMaquinaEscrever, velocidade);
+}
+
+// 3. START: Ativa o motor assim que a página carrega
+document.addEventListener("DOMContentLoaded", motorMaquinaEscrever);
